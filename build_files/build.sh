466a6a1af3b0841db3697e7f505cf642e43ecaa8
= <=>
2025-06-02 14:32:54 -0500

    remove gcr

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y distrobox gvfs-fuse gvfs-smb gcr
+dnf5 install -y distrobox gvfs-fuse gvfs-smb 
 
 # Use a COPR Example:
 #
